b5e594b6a8a87b1f0ada4fc0efd341ca026d9418
ifget <498702670@qq.com>
2023-04-11 22:27:36 +0800

    word

--- cmd/word.go
+++ cmd/word.go
@@ -1,13 +1,57 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"flagpro/internal/word"
+	"github.com/spf13/cobra"
+	"log"
+	"strings"
+)
+
+const (
+	ModeUpper = iota + 1
+	ModeLower
+	ModeUnderscoreToUpperCamelCase
+	ModeUnderscoreToLowerCamelCase
+	ModeCamelCaseToUnderscore
+)
+
+var desc = strings.Join([]string{
+	"改子命令支持各种格式转换:",
+	"1: UPPER",
+	"2: Lower",
+	"3: _ to UCC",
+	"4: _ to LCC",
+	"5: cc to _",
+}, "\n")
 
 var wordCmd = &cobra.Command{
 	Use:   "word",
 	Short: "单词格式转换",
-	Long:  "支持多种单词格式转换",
+	Long:  desc,
 	Run: func(cmd *cobra.Command, args []string) {
+		var content string
+		switch mode {
+		case ModeUpper:
+			content = word.ToUpper(str)
+		case ModeLower:
+			content = word.ToLower(str)
+		case ModeUnderscoreToUpperCamelCase:
+			content = word.UnderscoreToUpperCamelCase(str)
+		case ModeUnderscoreToLowerCamelCase:
+			content = word.UnderscoreToLowerCamelCase(str)
+		case ModeCamelCaseToUnderscore:
+			content = word.CamelCaseToUnderscore(str)
+		default:
+			log.Fatalf("暂不支持该转化模式，请执行 help word 查看帮助文档")
+		}
+		log.Printf("输出结果： %s", content)
 	},
 }
 
-func init() {}
+var str string
+var mode int8
+
+func init() {
+	wordCmd.Flags().StringVarP(&str, "str", "s", "", "请输入单词内容")
+	wordCmd.Flags().Int8VarP(&mode, "mode", "m", 0, "请输入单词的转换模式")
+}
